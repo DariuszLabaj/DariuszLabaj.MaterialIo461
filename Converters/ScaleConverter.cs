6229fd1778a4698da3a073ac197e245e0e01062b
DariuszLabaj <dareklabaj@gmail.com>
2024-10-06 11:28:29 +0200

    Add project files.

---
+++ Converters/ScaleConverter.cs
@@ -0,0 +1,45 @@
+﻿// Ignore Spelling: Labaj Dariusz
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace DariuszLabaj.MaterialIo461.Converters
+{
+    public class ScaleConverter : MarkupExtension, IValueConverter
+    {
+        private readonly double _scale;
+        public ScaleConverter(double scale)
+        {
+            _scale = scale;
+        }
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return DependencyProperty.UnsetValue;
+            if (double.TryParse(value.ToString(), out double inputValue))
+            {
+                return inputValue * _scale;
+            }
+            return DependencyProperty.UnsetValue;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return DependencyProperty.UnsetValue;
+            if (double.TryParse(value.ToString(), out double inputValuie))
+            {
+                return inputValuie / _scale;
+            }
+            return DependencyProperty.UnsetValue;
+        }
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
---
+++ CustomUserControl/SearchBox.xaml.cs
@@ -0,0 +1,43 @@
+﻿// Ignore Spelling: Labaj Dariusz
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DariuszLabaj.MaterialIo461.CustomUserControl
+{
+    /// <summary>
+    /// Interaction logic for SearchBox.xaml
+    /// </summary>
+    public partial class SearchBox : UserControl
+    {
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(SearchBox), new PropertyMetadata(string.Empty));
+        public static readonly DependencyProperty SearchPromptProperty = DependencyProperty.Register("SearchPrompt", typeof(string), typeof(SearchBox), new PropertyMetadata("Search"));
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public string SearchPrompt
+        {
+            get { return (string)GetValue(SearchPromptProperty); }
+            set { SetValue(SearchPromptProperty, value); }
+        }
+        public SearchBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
