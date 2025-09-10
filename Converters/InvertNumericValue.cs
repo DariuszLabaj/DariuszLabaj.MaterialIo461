d28e83efa1e37bf928ce9964500b269f15a79ea6
DariuszLabaj <dareklabaj@gmail.com>
2025-09-10 07:02:42 +0200

    Refactor project structure and add new UI components
    - Removed `Examples` directory from project file and excluded it from compilation.
    - Updated solution file to remove `MaterialIo48TestWpf` project.
    - Added `FilledTextInput` style for `TextBox` in `ButtonStyles.xaml`.
    - Introduced `GroupBoxStyleOutlined` style in `CardsStyles.xaml` with custom opacity masking.
    - Created `InvertNumericValue` and `ThicknessToHeightConverter` classes for data binding.
    - Implemented `TextBoxHelper` for managing labels with adorners in `TextBox` controls.
    - Added `TestingTextBox` page with sample `TextBox` controls demonstrating new features.

---
+++ Converters/InvertNumericValue.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace DariuszLabaj.MaterialIo461.Converters
+{
+    public class InvertNumericValue : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double numericValue)
+            {
+                return -numericValue;
+            }
+            else if (value is int intValue)
+            {
+                return -intValue;
+            }
+            else if (value is float floatValue)
+            {
+                return -floatValue;
+            }
+            else if (value is decimal decimalValue)
+            {
+                return -decimalValue;
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
